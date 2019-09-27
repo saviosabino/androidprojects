72bfa4e88b14912f4ed598ac2f63c6c56b764616
saviosabino <saviopp@gmail.com>
2019-09-26 22:21:07 -0300

    first

---
+++ andEmptyActvt/app/src/main/java/com/example/andemptyactvt/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.andemptyactvt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+    }
+
+}
